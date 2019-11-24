840c14cfd05e0c355298f6c2bc0e49592a8aa1d0
bass4x4 <ankushev.a@yandex.ru>
2019-11-24 21:03:01 +0300

    remove redundant panels.

--- src/main/java/Backend/App.java
+++ src/main/java/Backend/App.java
@@ -1,28 +1,18 @@
 package Backend;
 
-import UI.AuthWindow;
+import UI.AdminOptionsWindow;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 public class App {
     public static void main(String[] args) {
-        JFrame authWindow = new JFrame("Auth window");
-        authWindow.setContentPane(new AuthWindow().authPanel);
-        authWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        authWindow.pack();
-        authWindow.setSize(200, 200);
-        authWindow.setMinimumSize(new Dimension(200, 200));
-        PasswordUtils.initPasswords();
-        authWindow.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                PasswordUtils.savePasswordsToFile();
-                e.getWindow().dispose();
-            }
-        });
-        authWindow.setVisible(true);
+        JFrame cypherWindow = new JFrame("Auth window");
+        cypherWindow.setContentPane(new AdminOptionsWindow().getAdminOptionsPanel());
+        cypherWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cypherWindow.pack();
+        cypherWindow.setSize(350, 200);
+        cypherWindow.setMinimumSize(new Dimension(200, 200));
+        cypherWindow.setVisible(true);
     }
 }
