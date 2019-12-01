4f685abb9059b99899b2d381f7a41405db3b907e
bass4x4 <ankushev.a@yandex.ru>
2019-12-01 18:29:36 +0300

    comments.

--- src/main/java/Backend/RC4.java
+++ src/main/java/Backend/RC4.java
@@ -14,17 +14,18 @@ public class RC4 {
 
     private void init(byte[] key) {
         for (int i = 0; i < S.length; i++) {
-            S[i] = (byte) i;
+            S[i] = (byte) i; //инициализация ключевой последовательности числами 1...256 (65536)
+            // в зависимости от выбранного пользователем резмером S-блока - 8 (16) бит
         }
         int j = 0;
         for (int i = 0; i < S.length; i++) {
-            j = (j + getIntFromByte(S[i]) + key[i % key.length]) % S.length;
+            j = (j + getIntFromByte(S[i]) + key[i % key.length]) % S.length; //перемешивание ключевой последовательности при помощи заданной пользователем парольной фразы
             swap(S, i, j);
         }
     }
 
-    private byte keyItem() {
-        x = (x + 1) % S.length;
+    private byte nextByte() {
+        x = (x + 1) % S.length; //генерация последующего байта гаммы
         y = (y + getIntFromByte(S[x])) % S.length;
         swap(S, x, y);
         return S[(getIntFromByte(S[x]) + getIntFromByte(S[y])) % S.length];
@@ -33,7 +34,9 @@ public class RC4 {
     public byte[] Encode(byte[] data) {
         byte[] cipher = new byte[data.length];
         for (int i = 0; i < data.length; i++) {
-            cipher[i] = (byte) (data[i] ^ keyItem());
+            cipher[i] = (byte) (data[i] ^ nextByte()); //генерация результирующей последовательности путем выполнения операции XOR между
+            //ключом и данными. Для алгоритма RC4 операчия Encode полностью аналогична операции Decode, т.е. :
+            // M XOR K = C; C XOR K = M
         }
         return cipher;
     }
@@ -46,5 +49,5 @@ public class RC4 {
 
     private int getIntFromByte(byte b) {
         return b & 0xff;
-    }
+    } //из-за устройства JAVA приходится приводить тип byte к типу int для корректного обращения к массиву для избежания неверных индексов
 }
